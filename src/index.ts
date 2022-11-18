23c8d78e6814bd418a8fe1ea1f95a15e3140b2a4
Simen A. W. Olsen <so@bjerk.io>
2022-11-18 23:27:52 +0100

    add imports and remove extranous parent reference

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ import './iam';
 import './api-services';
 import './customers';
 import './bot';
+import './branches';
+import './identity-pool';
--- src/providers/core-google.ts
+++ src/providers/core-google.ts
@@ -1,10 +1,6 @@
 import * as gcp from '@pulumi/gcp';
 import { coreProject } from '../config';
 
-export const provider = new gcp.Provider(
-  'bjerk-core-google',
-  {
-    project: coreProject,
-  },
-  { parent: this },
-);
+export const provider = new gcp.Provider('bjerk-core-google', {
+  project: coreProject,
+});
