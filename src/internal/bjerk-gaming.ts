94b6e50080d36e550e5003728463a80551b39448
Simen A. W. Olsen <so@bjerk.io>
2022-11-27 20:30:03 +0100

    add api-services to bjerk-gaming

--- src/internal/bjerk-gaming.ts
+++ src/internal/bjerk-gaming.ts
@@ -1,3 +1,4 @@
+import * as gcp from '@pulumi/gcp';
 import { ProjectOnGithub } from '../components/projects-on-github';
 import { folder } from './folder';
 import { bjerkio } from '../github-orgs';
@@ -10,4 +11,36 @@ export const setup = new ProjectOnGithub(
     repository: 'gaming',
   },
   { providers: [bjerkio] },
+);
+
+export const services = [
+  'servicemanagement.googleapis.com',
+  'servicecontrol.googleapis.com',
+  'container.googleapis.com',
+  'compute.googleapis.com',
+  'dns.googleapis.com',
+  'cloudresourcemanager.googleapis.com',
+  'logging.googleapis.com',
+  'stackdriver.googleapis.com',
+  'monitoring.googleapis.com',
+  'cloudtrace.googleapis.com',
+  'clouderrorreporting.googleapis.com',
+  'clouddebugger.googleapis.com',
+  'cloudprofiler.googleapis.com',
+  'sqladmin.googleapis.com',
+  'cloudkms.googleapis.com',
+  'cloudfunctions.googleapis.com',
+  'run.googleapis.com',
+  'cloudbuild.googleapis.com',
+  'iam.googleapis.com',
+  'cloudbilling.googleapis.com',
+];
+
+export const apiServices = services.map(
+  (service) =>
+    new gcp.projects.Service(`bjerk-gaming-${service}`, {
+      service,
+      disableOnDestroy: false,
+      project: 'bjerk-gaming',
+    }),
 );
