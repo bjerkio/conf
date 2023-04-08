3d1fa67b777c66b4cc8b53ceb932c210a27b8b78
Simen A. W. Olsen <so@bjerk.io>
2023-04-08 11:49:17 +0200

    use configured github token

--- src/github-orgs.ts
+++ src/github-orgs.ts
@@ -1,17 +1,26 @@
+import * as pulumi from '@pulumi/pulumi';
 import * as github from '@pulumi/github';
+
+const config = new pulumi.Config('github');
+
+const token = config.requireSecret('token');
 
 export const bjerkio = new github.Provider(`bjerkio-provider`, {
   owner: 'bjerkio',
+  token,
 });
 
 export const basssene = new github.Provider(`basssene-provider`, {
   owner: 'basssene',
+  token,
 });
 
 export const branches = new github.Provider(`branches-provider`, {
   owner: 'getbranches',
+  token,
 });
 
 export const flexiSoft = new github.Provider(`flexi-soft-provider`, {
   owner: 'flexisoftorg',
-})
+  token,
+});
