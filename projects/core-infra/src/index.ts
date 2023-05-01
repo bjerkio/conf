c133a39563b20b39e788aa9ff521bb7741d8460c
Simen A. W. Olsen <so@bjerk.io>
2023-05-01 13:42:52 +0200

    wip

--- projects/core-infra/src/index.ts
+++ projects/core-infra/src/index.ts
@@ -1,9 +1,11 @@
 import './internal';
 import './foss';
 import './misc-projects';
-import './iam';
 import './api-services';
 import './customers';
 import './bot';
 import './branches';
 import './identity-pool';
+import { gitHubToken } from './stack-refs';
+
+export const token = gitHubToken;
