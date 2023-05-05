df49cbae3a182fc1239ddfad8f0483664fc8f727
Simen A. W. Olsen <so@bjerk.io>
2023-05-05 08:21:49 +0200

    wip

--- projects/core-infra/src/customers/abax-vwfs.ts
+++ projects/core-infra/src/customers/abax-vwfs.ts
@@ -7,6 +7,5 @@ const config = new pulumi.Config('abax-vwfs');
 export const setup = new ProjectOnGithub('abax-vwfs', {
   projectName: 'abax-vwfs-app',
   folderId: folder.id,
-  billingAccount: '0139CF-0C97EA-F3F6D9',
   owners: config.requireObject<string[]>('owners'),
 });
