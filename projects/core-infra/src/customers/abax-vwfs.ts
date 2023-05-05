23b6c21b626cc2629199a06b53306e0fe7c12b71
Simen A. W. Olsen <so@bjerk.io>
2023-05-05 08:32:57 +0200

    remove owners after 2023-05-12

--- projects/core-infra/src/customers/abax-vwfs.ts
+++ projects/core-infra/src/customers/abax-vwfs.ts
@@ -7,5 +7,8 @@ const config = new pulumi.Config('abax-vwfs');
 export const setup = new ProjectOnGithub('abax-vwfs', {
   projectName: 'abax-vwfs-app',
   folderId: folder.id,
-  owners: config.requireObject<string[]>('owners'),
+  owners:
+    // If the current date is before 2023-05-12, then use the owners from the config, if not remove them.
+    // We only give owner access for a limited time for security reasons.
+    Date.now() < 1683873069 ? config.requireObject<string[]>('owners') : [],
 });
