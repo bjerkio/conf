fc679bbebe75d7c7d0be41d9530b6855ae7557c2
Simen A. W. Olsen <so@bjerk.io>
2023-05-13 13:59:57 +0200

    remove owners from flexisoft

--- projects/core-infra/src/customers/flexisoft.ts
+++ projects/core-infra/src/customers/flexisoft.ts
@@ -3,8 +3,6 @@ import { ProjectOnGithub } from '../components/projects-on-github';
 import { flexiSoft } from '../github-orgs';
 import { folder } from './folder';
 
-const config = new pulumi.Config('flexisoft');
-
 export const setup = new ProjectOnGithub(
   'flexisoft',
   {
@@ -12,8 +10,6 @@ export const setup = new ProjectOnGithub(
     folderId: folder.id,
     repository: 'conf',
     billingAccount: '0139CF-0C97EA-F3F6D9',
-    // TODO: Restrict access
-    owners: config.requireObject<string[]>('owners'),
     addPulumiAccessToken: true,
   },
   { providers: [flexiSoft] },
