fa1e3b1222134379e2e34e547ab26ec3efdeafa3
Simen A. W. Olsen <so@bjerk.io>
2023-05-16 21:06:34 +0200

    add vault back

--- projects/bjerk-io/src/zones/bjerk-io.ts
+++ projects/bjerk-io/src/zones/bjerk-io.ts
@@ -119,6 +119,18 @@ new gcp.dns.RecordSet(
 );
 
 new gcp.dns.RecordSet(
+  'bjerk-io-vault',
+  {
+    managedZone,
+    name: 'vault.bjerk.io.',
+    rrdatas: ['34.88.68.230'],
+    ttl,
+    type: 'A',
+  },
+  opts,
+);
+
+new gcp.dns.RecordSet(
   'bjerk-io-reporting',
   {
     managedZone,
