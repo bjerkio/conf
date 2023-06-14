87b7a5e05ef8dfa9319b9daf91ae086ceda9da80
Simen A. W. Olsen <so@bjerk.io>
2023-06-14 09:03:19 +0200

    refactor: define roles, not owners
    We don't want the `roles/owner` role to be used for anything. If it is used, it should be

--- projects/core-infra/src/components/projects-on-github.ts
+++ projects/core-infra/src/components/projects-on-github.ts
@@ -2,13 +2,47 @@ import * as gcp from '@pulumi/gcp';
 import * as github from '@pulumi/github';
 import * as pulumi from '@pulumi/pulumi';
 import { interpolate } from '@pulumi/pulumi';
+import { isPast } from 'date-fns';
 import {
   billingAccount as defaultBillingAccount,
   pulumiAccessToken,
 } from '../config';
 import { provider as coreProvider } from '../providers/core-google';
-import { invariant } from '../utils';
+import { invariant, notEmpty } from '../utils';
 import { IdentityPoolGithubSetup } from './identity-pool-github';
+
+interface ProjectRole {
+  role: pulumi.Input<string>;
+  member: pulumi.Input<string>;
+
+  /**
+   * Required for admin roles
+   */
+  expiresAt?: pulumi.Input<Date>;
+}
+
+// Google administrator roles
+const adminRoles: string[] = [
+  'roles/owner',
+  'roles/resourcemanager.projectIamAdmin',
+  'roles/resourcemanager.folderIamAdmin',
+  'roles/resourcemanager.organizationAdmin',
+  'roles/billing.admin',
+  'roles/iam.organizationRoleAdmin',
+  'roles/iam.securityAdmin',
+  'roles/iam.serviceAccountAdmin',
+  'roles/iam.serviceAccountKeyAdmin',
+  'roles/iam.serviceAccountTokenCreator',
+  'roles/iam.roleAdmin',
+  'roles/iam.roleViewer',
+  'roles/iam.roleEditor',
+  'roles/iam.securityReviewer',
+  'roles/iam.workloadIdentityUser',
+  'roles/iam.workloadIdentityUserAdmin',
+  'roles/iam.workloadIdentityPoolAdmin',
+  'roles/iam.workloadIdentityPoolViewer',
+  'roles/iam.workloadIdentityPoolAdmin',
+];
 
 export interface ProjectOnGithubSpec {
   projectName?: pulumi.Input<string>;
@@ -32,7 +66,21 @@ export interface ProjectOnGithubSpec {
 
   serviceAccountRole?: pulumi.Input<string>;
 
+  /**
+   * Roles added with this will not be applied.
+   *
+   * @deprecated use projectRoles instead
+   */
   owners?: pulumi.Input<string>[];
+
+  /**
+   * Roles applied to the project
+   *
+   * Note: Roles should mostly be applied to spesific resources, not the project.
+   * Use this with caution.
+   *
+   */
+  roles?: pulumi.Input<ProjectRole>[];
 }
 
 export class ProjectOnGithub extends pulumi.ComponentResource {
@@ -59,7 +107,7 @@ export class ProjectOnGithub extends pulumi.ComponentResource {
       folderId,
       repositories = args.repository ? [args.repository] : [],
       projectAliases,
-      owners = [],
+      roles = [],
       addPulumiAccessToken = false,
       serviceAccountRole = 'roles/owner',
       billingAccount = defaultBillingAccount,
@@ -123,19 +171,42 @@ export class ProjectOnGithub extends pulumi.ComponentResource {
       );
     }
 
-    this.roles = pulumi.all(owners).apply(owners =>
-      owners.map(
-        member =>
-          new gcp.projects.IAMMember(
+    this.roles = pulumi.all(roles).apply(unwrappedRoles =>
+      unwrappedRoles
+        .map(({ member, role, expiresAt }) => {
+          if (adminRoles.includes(role)) {
+            if (!expiresAt) {
+              throw new Error(
+                `Admin roles require an expiresAt date. ${role} is missing expiresAt`,
+              );
+            }
+
+            if (isPast(expiresAt)) {
+              pulumi.log.warn(
+                `Admin role ${role} expires in the past. This will not be applied.`,
+              );
+              return;
+            }
+          }
+
+          if (expiresAt && isPast(expiresAt)) {
+            pulumi.log.warn(
+              `Role ${role} expires in the past. This will not be applied.`,
+            );
+            return;
+          }
+
+          return new gcp.projects.IAMMember(
             `${name}-owner-${member}`,
             {
               member,
-              role: 'roles/owner',
+              role,
               project: this.project.projectId,
             },
             { provider: this.googleProvider, parent: this },
-          ),
-      ),
+          );
+        })
+        .filter(notEmpty),
     );
 
     repositories.map(repository => {
