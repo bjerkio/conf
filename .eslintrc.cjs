07b9799e744f2977706cb17d12709ee3c91378b9
Simen A. W. Olsen <so@bjerk.io>
2023-07-14 00:46:13 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['@bjerk/eslint-config'],
+  rules: {
+    'import/no-unassigned-import': 'off',
+  },
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
