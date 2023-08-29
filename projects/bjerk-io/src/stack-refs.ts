a70fccb98d5f9f8705cbd73de1dd3a1a6471d4d2
Brage Sekse Aarset <brage@bjerk.io>
2023-08-29 14:35:23 +0300

    fix: set temporary local github token

--- projects/bjerk-io/src/stack-refs.ts
+++ projects/bjerk-io/src/stack-refs.ts
@@ -1,7 +1,10 @@
 import * as pulumi from '@pulumi/pulumi';
 
-const bjerkBot = new pulumi.StackReference('bjerk/bjerk-bot/prod');
+// const bjerkBot = new pulumi.StackReference('bjerk/bjerk-bot/prod');
 
-export const bjerkBotGitHubToken = pulumi.secret(
-  bjerkBot.requireOutput('gitHubToken'),
-);
+// export const bjerkBotGitHubToken = pulumi.secret(
+//   bjerkBot.requireOutput('gitHubToken'),
+// );
+
+const config = new pulumi.Config();
+export const bjerkBotGitHubToken = config.requireSecret('temp-github-token');
