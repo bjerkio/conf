250c23fed251a8f573b1139f1c6050ea823e3de1
Brage Sekse Aarset <brage@bjerk.io>
2023-08-29 14:56:54 +0300

    fix: remove unnecessary import

--- projects/bjerk-io/src/github.ts
+++ projects/bjerk-io/src/github.ts
@@ -1,5 +1,4 @@
 import * as github from '@pulumi/github';
-import * as pulumi from '@pulumi/pulumi';
 import { bjerkBotGitHubToken } from './stack-refs';
 
 export const githubProvider = new github.Provider('gh-provider', {
