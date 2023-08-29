72b39c1eaaed1560679769f5424ef71fc87bf8bd
Brage Sekse Aarset <brage@bjerk.io>
2023-08-29 14:42:21 +0300

    move out of stack ref

--- projects/bjerk-io/src/github.ts
+++ projects/bjerk-io/src/github.ts
@@ -1,7 +1,10 @@
 import * as github from '@pulumi/github';
-import { bjerkBotGitHubToken as token } from './stack-refs';
+import * as pulumi from '@pulumi/pulumi';
+
+const config = new pulumi.Config();
+export const bjerkBotGitHubToken = config.requireSecret('temp-github-token');
 
 export const githubProvider = new github.Provider('gh-provider', {
   owner: 'bjerkio',
-  token,
+  token: bjerkBotGitHubToken,
 });
