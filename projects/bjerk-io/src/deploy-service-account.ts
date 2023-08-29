c46b808de00f0c1d163e1674f9152149255c6fab
Brage Sekse Aarset <brage@bjerk.io>
2023-08-29 09:19:47 +0300

    revert format change

--- projects/bjerk-io/src/deploy-service-account.ts
+++ projects/bjerk-io/src/deploy-service-account.ts
@@ -17,11 +17,14 @@ new gcp.projects.IAMMember('deploy-firebase-iam', {
   role: 'roles/firebasehosting.admin',
 });
 
-new github.ActionsSecret('deploy-url', {
-  secretName: 'GOOGLE_PROJECT_SA_KEY',
-  plaintextValue: saKey.privateKey.apply(k =>
-    Buffer.from(k, 'base64').toString('utf-8'),
-  ),
-  repository: 'website',
-  { provider: githubProvider }
-});
+new github.ActionsSecret(
+  'deploy-url',
+  {
+    secretName: 'GOOGLE_PROJECT_SA_KEY',
+    plaintextValue: saKey.privateKey.apply(k =>
+      Buffer.from(k, 'base64').toString('utf-8'),
+    ),
+    repository: 'website',
+  },
+  { provider: githubProvider },
+);
