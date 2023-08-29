7e9571e8089206f38af086e9670806e45c7d5cf0
Brage Sekse Aarset <brage@bjerk.io>
2023-08-29 15:14:55 +0300

    fix token name

--- projects/core-infra/src/stack-refs.ts
+++ projects/core-infra/src/stack-refs.ts
@@ -3,5 +3,5 @@ import * as pulumi from '@pulumi/pulumi';
 const bjerkBot = new pulumi.StackReference('bjerk/bjerk-bot/prod');
 
 export const bjerkBotGitHubToken = pulumi.secret(
-  bjerkBot.requireOutput('gitHubToken'),
+  bjerkBot.requireOutput('githubToken'),
 );
